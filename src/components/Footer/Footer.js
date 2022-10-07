07d2cb7b27dc476d05835c6ab4a61739aaea286d
SaiRao1997 <saibittu.rao@gmail.com~>
2022-10-07 20:21:04 +0530

    Quiz APP

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      Made with ♥ by{" "}
+      <a href="#" style={{ cursor: "pointer" }}>
+        Sai Rao
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
